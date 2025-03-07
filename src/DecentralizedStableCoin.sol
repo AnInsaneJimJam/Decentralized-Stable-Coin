209a76a77018592cb3c575b67941379f532e2b40
12anand34 <anand47bansal#gmail.com>
2025-03-07 16:23:19 +0000

    format

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -25,8 +25,8 @@
 
 pragma solidity ^0.8.18;
 
-import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
 * @title DecentralizedStableCoin
@@ -36,38 +36,34 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 *
 * This is the contract meant to be governed by DSCEngine. This is ERC20 implementation of our stablecoin system.
 */
-contract DecentralizedStableCoin is ERC20Burnable,Ownable{
-
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DSC_MustBeMoreThanZero();
     error DSC_BurnAmountExceedsBalance();
     error DSC_InvalidAddress();
 
     //0x87 is a mock address just to avoid error for now. Later put DSC engine contract address
 
-    constructor() ERC20("DecentralizedStableCoin","DSC") Ownable(address(0x87)){}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(address(0x87)) {}
 
-    function burn(uint256 _amount) public override onlyOwner{
+    function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
-        if(_amount <= 0){
+        if (_amount <= 0) {
             revert DSC_MustBeMoreThanZero();
         }
-        if(balance < _amount){
+        if (balance < _amount) {
             revert DSC_BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
 
-    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
-        if(_to == address(0)){
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert DSC_InvalidAddress();
         }
-        if(_amount <=0){
+        if (_amount <= 0) {
             revert DSC_MustBeMoreThanZero();
         }
-        _mint(_to,_amount);
+        _mint(_to, _amount);
         return true;
     }
-
-
 }
-
